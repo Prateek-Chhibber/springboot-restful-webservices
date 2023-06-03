da58cbe6f08ee82fdc2a6f792a29d7c17bb95527
prateekchhibber <0392pc@gmail.com>
2023-06-04 00:39:06 +0530

    Build Get All Users Rest API

--- src/main/java/com/toxicprogrammer/springboot/service/UserService.java
+++ src/main/java/com/toxicprogrammer/springboot/service/UserService.java
@@ -2,9 +2,13 @@ package com.toxicprogrammer.springboot.service;
 
 import com.toxicprogrammer.springboot.entity.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User createUser(User user);
 
     User getUserById(Long userId);
+
+    List<User> getAllUsers();
 }
