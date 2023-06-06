d857139f06a4831ee62256b8040d3b2a01f25ad0
prateekchhibber <0392pc@gmail.com>
2023-06-06 23:30:28 +0530

    Customising Swagger API documentation with Annotations

--- src/main/java/com/toxicprogrammer/springboot/controller/UserController.java
+++ src/main/java/com/toxicprogrammer/springboot/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.toxicprogrammer.springboot.controller;
 
 import com.toxicprogrammer.springboot.dto.UserDto;
-import com.toxicprogrammer.springboot.entity.User;
 import com.toxicprogrammer.springboot.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,11 +16,24 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/users")
+@Tag(
+        name = "CRUD REST APIs for User Resource",
+        description = "CRUD REST APIs - Create user, get user, get all users, update user and delete user"
+)
 public class UserController {
 
     private UserService userService;
 
 //    Build create User API
+    @Operation(
+            summary = "Create User REST API",
+            description = "Save user to database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 created"
+
+    )
     @PostMapping()
     public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto user){
        UserDto savedUser = userService.createUser(user);
@@ -27,6 +42,15 @@ public class UserController {
 
 //    build get user by id REST API
 //    http://localhost:8080/api/users/1
+@Operation(
+        summary = "Get User REST API",
+        description = "Get user from database"
+)
+@ApiResponse(
+        responseCode = "200",
+        description = "HTTP Status 200 retrieved"
+
+)
     @GetMapping("{id}")
     public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long userId){
         UserDto user = userService.getUserById(userId);
@@ -35,6 +59,15 @@ public class UserController {
 
 //    Build GET all users REST API
 //    http://localhost:8080/api/users
+@Operation(
+        summary = "Get All Users REST API",
+        description = "Get All Users from database"
+)
+@ApiResponse(
+        responseCode = "200",
+        description = "HTTP Status 200 SUCCESS"
+
+)
     @GetMapping
     public ResponseEntity<List<UserDto>> getAllUsers(){
         List<UserDto> users = userService.getAllUsers();
@@ -43,6 +76,15 @@ public class UserController {
 
 //    Build Update User REST API
     @PutMapping("{id}")
+    @Operation(
+            summary = "Update User REST API",
+            description = "Update user and save to database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+
+    )
     public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long userId, @Valid @RequestBody UserDto user){
         user.setId(userId);
         UserDto  updatedUser = userService.updateUser(user);
@@ -50,6 +92,15 @@ public class UserController {
     }
 
 //    Build Delete User REST API
+@Operation(
+        summary = "Delete User REST API",
+        description = "Delete user from database"
+)
+@ApiResponse(
+        responseCode = "200",
+        description = "HTTP Status 200 SUCCESS"
+
+)
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteUser(@PathVariable("id") Long userId){
         userService.deleteUser(userId);
